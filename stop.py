ed256836e4e429b9300adda58bdd64327c5c25f1
Kanisorn-S <karamahati@gmail.com>
2024-03-16 09:00:23 +0700

    k

--- stop.py
+++ stop.py
@@ -1,3 +1,15 @@
 from motor import stop
+import Adafruit_PCA9685
+
+# Initialize PCA
+servo = Adafruit_PCA9685.PCA9685(address = 0x40, busnum = 1)
+servo.set_pwm_freq(60)
+
+# Set motor channel
+motor_channel_left = 0
+motor_channel_right = 1
+motor_unload = 2
+
+stop(servo, motor_channel_left, motor_channel_right)
 
 
