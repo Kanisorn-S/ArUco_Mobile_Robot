9e367dd5f908c649721b87572a0edd91e5a03cdb
Kanisorn-S <karamahati@gmail.com>
2024-03-16 09:05:03 +0700

    stop

--- startup.py
+++ startup.py
@@ -20,15 +20,17 @@ import sys
 import importlib
 from god import god
 
-def main():
-    # Initialize PCA
-    servo = Adafruit_PCA9685.PCA9685(address = 0x40, busnum = 1)
-    servo.set_pwm_freq(60)
+# Initialize PCA
+servo = Adafruit_PCA9685.PCA9685(address = 0x40, busnum = 1)
+servo.set_pwm_freq(60)
 
-    # Set motor channel
-    motor_channel_left = 0
-    motor_channel_right = 1
-    motor_unload = 2
+# Set motor channel
+motor_channel_left = 0
+motor_channel_right = 1
+motor_unload = 2
+
+def main():
+    
 
     # Set default wheel speed
     v = 3
@@ -114,4 +116,8 @@ def main():
     f.close()
 
 if __name__ == "__main__":
-     main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        stop(servo, motor_channel_left, motor_channel_right)
+        sys.exit()
